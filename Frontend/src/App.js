d0051d71027fe9d459b8e9cc89bb6a73119151d5
sleepyfoodie <cerynaa@gmail.com>
2017-10-17 02:35:49 -0400

    emotions are returning correctly

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -28,8 +28,16 @@ class App extends Component {
       "threshold": 0.1
     });
     promise.then((result) => {
-      console.log(result.data);
-      console.log(this.state.emotion);
+      console.log(result.data.results);
+      var emotions = result.data.results;
+      var emotion;
+      Object.keys(emotions).reduce(function(a,b) {
+        return emotion = emotions[a] > emotions[b] ? a : b
+      })
+      this.setState({
+        feeling: emotion,
+        emotion: ""
+      })
     });
     promise.catch((error) => {
       console.log(error);
@@ -39,40 +47,36 @@ class App extends Component {
     return (
       <div>
         <Emotion emotion={this.state.emotion} onChange={this.handleChange} submit={this.submit}/>
-        <Feeling emotion={this.state.emotion}/>
+        <Feeling emotion={this.state.feeling}/>
       </div>
     )
   }
 }
 
 //This component holds the box where user enters a string of text and submit it into an API for lexical analytics.
-class Emotion extends Component {
-  render() {
-    return (
-      <div className="Emotion">
-        <form onSubmit={(e) => { this.props.submit(e) }} type="submit">
-          <textarea
-            id="emotion"
-            required
-            value={this.props.emotion}
-            onChange={(e) => { this.props.onChange(e) }}>
-          </textarea>
-          <button onClick={this.props.submit}>click</button>
-        </form>
-      </div>
-    );
-  }
+function Emotion(props) {
+  return (
+    <div>
+      <form onSubmit={(e) => {props.submit(e)} } type="submit">
+        <textarea
+          id="emotion"
+          required
+          value={props.emotion}
+          onChange={(e) => {props.onChange(e)} }>
+        </textarea>
+        <button onClick={props.submit}>enter</button>
+      </form>
+    </div>
+  )
 }
 
 //This component shows what the user is feeling
-class Feeling extends Component {
-  render() {
-    return (
-      <div>
-        You are feeling: {this.props.emotion}
-      </div>
-    )
-  }
+function Feeling(props) {
+  return (
+    <div>
+      You are feeling: {props.emotion}
+    </div>
+  )
 }
 
 export default App;
